77ce055df45b92cb8fd240f95b5a5c191946365f
Sylvie-B <unsolo@hotmail.fr>
2022-01-21 10:28:04 +0100

    work on file

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$lorem = "Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.";
+file_put_contents('monLorem.txt', $lorem);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,20 +16,42 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$arrString = [
+    'perspiciatis ',
+    'voluptatem ',
+    'accusantium ',
+    'doloremque ',
+    'laudantium',
+    'totam ',
+    'aperiam ',
+    'eaque ',
+    'inventore ',
+    'architecto ',
+];
+
+foreach ($arrString as $string){
+    file_put_contents('monLorem.txt', $string, FILE_APPEND);
+}
 
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
+$file = 'monLorem.txt';
+$info = pathinfo($file);
 
+echo "Extension : " . $info['extension'] . "<br>";
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+$file = 'toto.txt';
+if(!file_exists($file)){
+    echo "le fichier n'existe pas";
+}
 
 /**
  * Super bonus.
@@ -36,3 +59,13 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+
+$file = fopen('monLorem.txt', 'r+');
+while ($char = fgetc($file)){
+    if($char === "a"){
+        fseek($file, ftell($file)-1);
+        fwrite($file, "@", 1);
+    }
+}
+
+fclose($file);
